18529cf238f0f314cd1d5896e3bb5a8fd1a3fbda
TME <tyler.s.meyers@gmail.com>
2024-04-16 16:07:05 -0600

    routes checked and running

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const { User, Artist, UserRecord, Record } = require("../../models");
 
+//http://localhost:3001/api/users
+router.get("/", async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      include: [
+        {
+          model: Record,
+          through: UserRecord,
+          as: "user_records",
+        },
+      ],
+    });
+
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 //http://localhost:3001/api/users/:id
 router.get("/:id", async (req, res) => {
   try {
